5ad8887fba359051d364a100cbb92ecea624106c
Augusto Buccolini <buccoliniaugusto@gmail.com>
2024-11-11 21:46:09 -0300

    implemente 2 funciones de suma

---
+++ ejemplo1.py
@@ -0,0 +1,10 @@
+def saludando():
+    print("hola chicos estoy trabajando con git")
+    
+saludando()
+
+def sumar(n1, n2):
+    print("el resultado de la suma es ", n1 + n2)
+    
+sumar(int(input("ingresa el primer numero")),int(input("ingresa el seg numero")))
+
